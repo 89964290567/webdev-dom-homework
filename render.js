246ef3cd698acc1abe6728e5ac3a0312fa20b827
hw1 <lilia_dali@mail.ru>
2023-04-13 03:26:36 +0700

    hw2.16

--- render.js
+++ render.js
@@ -1,29 +1,31 @@
-import {checkClick} from "./script.js"
-function renderComments(comments, listElement) {
-    const commentsHtml = comments.map((user, index) => {
-        return `<li id="list" class="comment">
-          <div class="comment-header">
-            <div>${user.name}</div>
-            <div>${user.date}</div>
-          </div>
-          <div class="comment-body"> 
-             <div class="comment-text" data-index="${index}">
-                ${user.text}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <button data-index="${index}" class="delete-button">Удалить</button>
-            <div class="likes">
-              <span class="likes-counter">${user.likesCount}</span>
-              <button data-index="${index}"  class="${user.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
-            </div> 
-          </div>
-        </li>`;
-    }).join("");
+import { click } from "./script.js";
+function registerUser(registerElement,checkEnter) {
+    
+    switch (checkEnter) {
+        case "enter":
+            registerElement.innerHTML = `
+        <input id="login" type="text" placeholder="Введите логин" >
+        <input id="password" type="text" placeholder="Введите пароль" >
+        <button id="enterButton" class="add-form-button">Войти</button>
+        <button id="registrButton" class="add-form-button">Зарегистрироваться</button>
+        `
+            break;
+        case "registr":
+            registerElement.innerHTML = `
+        <input id="name" type="text" placeholder="Введите имя" >
+        <input id="login" type="text" placeholder="Введите логин" >
+        <input id="password" type="text" placeholder="Введите пароль" >
+        <button id="registrButton" class="add-form-button">Зарегистрироваться</button>
+        <button id="enterButton" class="add-form-button">Войти</button>
+        
+    `
+            break;
+        default:
+            break;
+    }
+    click();
+}
 
-    listElement.innerHTML = commentsHtml;
 
-    checkClick(); // Здесь вызывается функция (находиться на 123 строчке) которая отслеживает нажатия на все кнопки, каждый раз после рендера.
-};
-export { renderComments }; // import пишем в начале кода а export в конец.
 
+export {registerUser};
